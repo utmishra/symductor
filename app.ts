8c3938c2f94e590b735abe027d70b5ae375b08ec
Utkarsh Mishra <utmishra@gmail.com>
2023-02-05 01:52:54 +0100

    Using Typescript, ESLint & Prettier

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
+import createError from 'http-errors';
+import express, { Express, Request, Response } from 'express';
+import * as path from 'path';
+import cookieParser from 'cookie-parser';
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -25,17 +25,6 @@ app.use('/users', usersRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
 });
 
 module.exports = app;
