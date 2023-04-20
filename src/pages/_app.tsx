6efff863cfbeee5ad541c72c88c529784ab1569a
Utkarsh Mishra <utkarsh.mishra@frontify.com>
2023-04-20 03:24:14 +0200

    Daily & Last Week Productivity Score

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,8 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { createTheme, NextUIProvider } from '@nextui-org/react';
+import { Inconsolata } from 'next/font/google';
 import React from 'react';
-
-if (typeof window === 'undefined') {
-  React.useLayoutEffect = React.useEffect;
-}
 
 const theme = createTheme({
   type: 'light', // it could be "light" or "dark"
@@ -18,15 +15,19 @@ const theme = createTheme({
   },
 });
 
+const inconsolata = Inconsolata({ subsets: ['latin'] });
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <NextUIProvider theme={theme}>
-      <Component
-        {...pageProps}
-        style={{
-          background: '#ffe2e2',
-        }}
-      />
+      <main className={inconsolata.className}>
+        <Component
+          {...pageProps}
+          style={{
+            background: '#ffe2e2',
+          }}
+        />
+      </main>
     </NextUIProvider>
   );
 }
--- src/pages/rescuetime/RescueTimeProductivityScore.tsx
+++ src/pages/rescuetime/RescueTimeProductivityScore.tsx
@@ -3,13 +3,26 @@ import { useRescueTimeData } from './hook';
 import { useMemo } from 'react';
 import { RescueTimeDailySummaryFeed } from '@/types/rescuetime';
 
-const renderProgressBar = (status: string, data?: RescueTimeDailySummaryFeed[] | undefined | null, error?: string) => {
+const getProgressColor = (value: number): 'error' | 'warning' | 'primary' | 'success' => {
+  switch (true) {
+    case value < 60:
+      return 'error';
+    case value < 75:
+      return 'warning';
+    case value < 90:
+      return 'primary';
+    default:
+      return 'success';
+  }
+};
+
+const renderProgressBar = (status: string, productivityScore: number | null, error?: string) => {
   switch (status) {
     case 'success':
-      if (data && data[0].productivity_pulse) {
-        return <Progress striped value={data[0].productivity_pulse} />;
+      if (productivityScore !== null) {
+        return <Progress color={getProgressColor(productivityScore)} striped value={productivityScore} />;
       } else {
-        return <Tooltip color='error' content='Something went wrong' />;
+        return <Tooltip color='error' content={error} />;
       }
     case 'error':
       return <Tooltip color='error' content={error} />;
@@ -20,8 +33,23 @@ const renderProgressBar = (status: string, data?: RescueTimeDailySummaryFeed[] |
   }
 };
 
+const thisWeekOrLastWeekScore = (data: RescueTimeDailySummaryFeed[]): number => {
+  const itsEarlyWeek = new Date().getDay() < 4;
+  if (itsEarlyWeek) {
+    return data.slice(1, 8).reduce((acc, curr) => acc + curr.productivity_pulse, 0) / (data.length - 1);
+  } else {
+    return Math.floor(
+      data
+        .filter((currentData) => {
+          return new Date(currentData.date).getDay() > 3;
+        })
+        .reduce((acc, curr) => acc + curr.productivity_pulse, 0) / data.length,
+    );
+  }
+};
+
 export const RescueTimeProductivityScore = () => {
-  const { data, isLoading, isError } = useRescueTimeData();
+  const { data, isLoading, isError }: { data: null | RescueTimeDailySummaryFeed[]; isLoading: boolean; isError: string } = useRescueTimeData();
 
   const cardType = useMemo(() => {
     if (isError) {
@@ -36,13 +64,35 @@ export const RescueTimeProductivityScore = () => {
   }, [data, isLoading, isError]);
 
   return (
-    <Card>
-      <Card.Header>Your Productivity Score today</Card.Header>
-      <Card.Body>
-        <Row justify='center' align='center'>
-          <Col span={12}>{renderProgressBar(cardType, data, isError)}</Col>
-        </Row>
-      </Card.Body>
-    </Card>
+    <Row justify='center' align='center' gap={1}>
+      <Col span={3}>
+        <Card>
+          <Card.Header>Your Productivity Score today: {data !== null && data[0] ? Math.floor(data[0]['productivity_pulse']) : ''}%</Card.Header>
+          <Card.Body>
+            <Row justify='center' align='center'>
+              <Col span={12}>
+                {renderProgressBar(
+                  data !== null && data[0] ? cardType : 'error',
+                  data !== null && data[0] ? data[0]['productivity_pulse'] : null,
+                  isError,
+                )}
+              </Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+      <Col span={3}>
+        <Card>
+          <Card.Header>
+            Your Productivity Score {new Date().getDay() < 4 ? 'this week' : 'last week'}: {data !== null ? thisWeekOrLastWeekScore(data) : ''}%
+          </Card.Header>
+          <Card.Body>
+            <Row justify='center' align='center'>
+              <Col span={12}>{renderProgressBar(cardType, data !== null && data[0] ? thisWeekOrLastWeekScore(data) : null, isError)}</Col>
+            </Row>
+          </Card.Body>
+        </Card>
+      </Col>
+    </Row>
   );
 };
