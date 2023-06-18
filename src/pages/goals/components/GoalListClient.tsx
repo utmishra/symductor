047ab059294a2aebc3b22a0285653d15245b1f2a
Utkarsh Mishra <utkarsh.mishra@frontify.com>
2023-06-18 17:42:21 +0200

    Update GoalListClient.tsx
    Temporarily not exporting GoalList

--- src/pages/goals/components/GoalListClient.tsx
+++ src/pages/goals/components/GoalListClient.tsx
@@ -4,7 +4,7 @@ import { Text } from '@nextui-org/react';
 import { addGoal, addSubgoal, updateGoal, updateSubgoal, deleteGoal, deleteSubgoal } from '@/services/goals';
 import Goal from './Goal';
 
-export const GoalListClient: React.FC = () => {
+const GoalListClient: React.FC = () => {
   const { goals, isLoading, error } = useGoals();
 
   if (isLoading) {
